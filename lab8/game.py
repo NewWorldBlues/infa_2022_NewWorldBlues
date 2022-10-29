5c9e6ce2eab9a52466fadb72b7aafd32b72cad19
Trubeev Sergei <trubeev7@gmail.com>
2022-10-29 12:21:09 +0300

    Added score

--- lab8/game.py
+++ lab8/game.py
@@ -7,6 +7,7 @@ def new_ball():
     """Draws a new circle of random color and radius
     at the random position on the screen.
     """
+    global x, y, r
     x = randint(100, 1100)
     y = randint(100, 900)
     r = randint(10, 100)
@@ -14,34 +15,48 @@ def new_ball():
     circle(screen, color, (x, y), r)
 
 
-def quit_detector(event):
-    """Detects clicks on the mouse button or quitting the game.
-    Returns True, if the event is quitting,
-    and False, if the event is click.
+def hit_registration(x1, y1,
+                     r1, event,
+                     points):
+    """Checks the hitting into the circle.
+    Adds a point for hitting into the circle.
 
-    :param event: occurred event of click or quitting
+    :param x1, y1: coordinates of the center of the circle
+    :param r1: radius of the circle
+    :param event: occurred event of click
+    :param points: initial score
     """
-    if event.type == pygame.QUIT:
-        return True
-    elif event.type == pygame.MOUSEBUTTONDOWN:
-        print('Click!')
-        return False
+    if (event.pos[0] - x1)**2 + (event.pos[1] - y1)**2 <= r1**2:
+        print('Hit!')
+        return points + 1
+    else:
+        print('Miss!')
+        return points
 
 
-def main_algorithm(finished, frequency):
+def main_algorithm(finished, frequency, points):
     """Spawns random circles at the screen during every defined period of time.
-    With every click function prints 'Click!' in the console.
+    With every hitting into the circle prints "Hit!" and adds one point to the
+    score.
+    With every missing prints "Miss!"
+    After closure of the window prints player's score
 
     :param finished: parameter that shows the closure of screen
     :param frequency: frequency of changing of circles
+    :param points: initial score
     """
     while not finished:
         clock.tick(frequency)
         for event in pygame.event.get():
-            finished = quit_detector(event)
+            if event.type == pygame.QUIT:
+                finished = True
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                points = hit_registration(x, y, r, event, points)
+                break
         new_ball()
         pygame.display.update()
         screen.fill(BLACK)
+    print('Your score is:', points)
 
 
 pygame.init()
@@ -63,8 +78,8 @@ COLORS = [
 pygame.display.update()
 clock = pygame.time.Clock()
 starting_argument = False
-FPS = 2
+FPS = 0.25
 
-main_algorithm(starting_argument, FPS)
+main_algorithm(starting_argument, FPS, 0)
 
 pygame.quit()
